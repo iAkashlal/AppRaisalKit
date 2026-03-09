6224c8cbc4fecf29d18010fbc4ef214bb7dea8cb
Akashlal Bathe <tronicxo@gmail.com>
2026-03-09 09:56:20 +0530

    Sample app initialization

---
+++ AppRaisalKit Sample/AppRaisalKit Sample/AppRaisalKit_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppRaisalKit_SampleApp.swift
+//  AppRaisalKit Sample
+//
+//  Created by Akashlal Bathe on 09/03/26.
+//
+
+import SwiftUI
+
+@main
+struct AppRaisalKit_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
